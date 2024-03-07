c1f2488bccc5b29ea057a0f9d842f58695065ac4
drimh <107299318+drimh@users.noreply.github.com>
2024-03-07 15:11:08 +0900

    feat: 통신 테스트

--- agile-hub/src/App.js
+++ agile-hub/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [responseText, setResponseText] = useState('');
+
+  useEffect(() => {
+    const apiUrl = 'http://www.agilehub.store/api/health'; // 서버 주소로 변경
+
+    axios.get(apiUrl)
+      .then(response => {
+        setResponseText(response.data);
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>API Response:</h1>
+      <p>{responseText}</p>
     </div>
   );
 }
+
 
 export default App;
