a36dd1d95c27770110ec6b921924f52c0ab3c8ca
ju wonhee <jwonhee398@gmail.com>
2024-03-20 22:10:08 +0900

    feat:5 라우트 설정 완료, /login 접속 시 로그인 페이지

--- agile-hub/src/App.js
+++ agile-hub/src/App.js
@@ -1,7 +1,5 @@
-//import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
-//import axios from 'axios';
 import MainPage from './components/MainPage/MainPage.js'; // 로그인 페이지 컴포넌트
 import LoginPage from './components/LoginPage/LoginPage.js'; // 로그인 페이지 컴포넌트
 
--- agile-hub/src/components/LoginPage/LoginPage.js
+++ agile-hub/src/components/LoginPage/LoginPage.js
@@ -1,6 +1,5 @@
 // LoginPage.js
 import React from 'react';
-//import { Slide } from 'react-slideshow-image'; // 라이브러리 import, yarn add react-slideshow-image
 import '../../css/LoginPage/Login.css';
 import Slider from './Slider';
 
