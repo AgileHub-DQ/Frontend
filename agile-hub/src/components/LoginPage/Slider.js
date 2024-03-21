f36e5ee3f516b8312a3ab90cbe400dfd7e1c8144
ju wonhee <jwonhee398@gmail.com>
2024-03-21 13:14:56 +0900

    feat:5 LoginPage, Slider 컴포넌트 크기 조절 완료

--- agile-hub/src/components/LoginPage/Slider.js
+++ agile-hub/src/components/LoginPage/Slider.js
@@ -4,17 +4,17 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css'; // 슬라이더 
 
 const Slider = () => {
   return (
-    <Carousel autoPlay interval={2000} infiniteLoop>
+    <Carousel autoPlay interval={2000} infiniteLoop >
       <div>
-        <img src="https://via.placeholder.com/600x400" alt="slide1" />
+        <img src="https://via.placeholder.com/600x550" alt="slide1" />
         <p className="legend">어떤 환경에서든 AgileHub와 함께!</p>
       </div>
       <div>
-        <img src="https://via.placeholder.com/600x400" alt="slide2" />
+        <img src="https://via.placeholder.com/600x550" alt="slide2" />
         <p className="legend">애자일을 원하는 모든 이를 위한 최적의 선택!</p>
       </div>
       <div>
-        <img src="https://via.placeholder.com/600x400" alt="slide3" />
+        <img src="https://via.placeholder.com/600x550" alt="slide3" />
         <p className="legend">프로젝트 관리의 혁신, 간편하고 명확하게!</p>
       </div>
     </Carousel>
