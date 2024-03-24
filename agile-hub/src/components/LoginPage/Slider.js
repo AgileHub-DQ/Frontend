1d0c9fc66be043d88ea4438815e1b7d56b7b4bc0
ju wonhee <jwonhee398@gmail.com>
2024-03-24 19:01:18 +0900

    feat:5 마지막 슬라이드 안 나오는 오류 해결
    필요 이상으로 css 코드 넣어서 오류 발생 -> 코드 삭제하여 해결

--- agile-hub/src/components/LoginPage/Slider.js
+++ agile-hub/src/components/LoginPage/Slider.js
@@ -1,4 +1,3 @@
-// Slider.js
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import '../../css/LoginPage/Slider.css'; // 슬라이더 텍스트 css, 슬라이더 css보다 위에 import 해주어야 함
@@ -6,21 +5,22 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css'; //슬라이더 c
 
 const Slider = () => {
   return (
-    <Carousel autoPlay interval={2000} infiniteLoop showIndicators showArrows>
-      <div>
-        <img src="https://via.placeholder.com/600x550" alt="slide1" />
-        <p className="legend">어떤 환경에서든 AgileHub와 함께!</p>
-      </div>
-      <div>
-        <img src="https://via.placeholder.com/600x550" alt="slide2" />
-        <p className="legend">애자일을 원하는 모든 이를 위한 최적의 선택!</p>
-      </div>
-      <div>
-        <img src="https://via.placeholder.com/600x550" alt="slide3" />
-        <p className="legend">프로젝트 관리의 혁신, 간편하고 명확하게!</p>
-      </div>
+    <Carousel autoPlay interval={5000} infiniteLoop showIndicators showArrows={false} showThumbs={false}>
+        <div>
+          <img src="https://via.placeholder.com/600x550" alt="slide1" />
+          <p className="legend">어떤 환경에서든 AgileHub와 함께!</p>
+        </div>
+        <div>
+          <img src="https://via.placeholder.com/600x550/000000/FFFFFF?text=Slide+2" alt="slide2" />
+          <p className="legend">애자일을 원하는 모든 이를 위한 최적의 선택!</p>
+        </div>
+        <div>
+          <img src="https://via.placeholder.com/600x550/FFFFFF/000000?text=Slide+3" alt="slide3" />
+          <p className="legend">프로젝트 관리의 혁신, 간편하고 명확하게!</p>
+        </div>
     </Carousel>
   );
 };
 
 export default Slider;
+
