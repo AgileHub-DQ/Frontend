2c0e648abec1a32e6ec322fd70c08142882e7045
ju wonhee <jwonhee398@gmail.com>
2024-03-24 23:53:45 +0900

    feat:5 package.json에 proxy 코드 추가 & 메인페이지 async 코드로 수정

--- agile-hub/src/components/MainPage/MainPage.js
+++ agile-hub/src/components/MainPage/MainPage.js
@@ -3,29 +3,34 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function MainPage() {
-  const [responseText, setResponseText] = useState('');
+  const [responseData, setResponseData] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    const apiUrl = "/api/health"; // 프록시 설정한 URL을 포함하지 않음
-    
-    axios.get(apiUrl)
-      .then(response => {
-        console.log(response.data);
-        setResponseText(response.data);
-      })
-      .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-      });
-    
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("/api/health");
+        setResponseData(response.data);
+      } catch (error) {
+        console.error('fetch error', error);
+        setError('API 요청에 문제 발생');
+      }
+    };
+
+    fetchData();
   }, []);
-  
+
   return (
     <>
       <h1>여기는 MainPage입니다.</h1>
-      <h2>API Response:{responseText}</h2>
+      {error && <p>Error: {error}</p>}
+      {responseData && (
+        <div>
+          <h2>API Response: {responseData}</h2>
+        </div>
+      )}
     </>
   );
-  
 }
 
 export default MainPage;
