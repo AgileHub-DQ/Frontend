fdb05bff0f818c359bb11fa256958f98eb18838e
drimh <107299318+drimh@users.noreply.github.com>
2024-04-02 14:50:28 +0900

    feat: 이슈 삭제 통신코드 완료

--- agile-hub/src/pages/SingleIssue.js
+++ agile-hub/src/pages/SingleIssue.js
@@ -4,9 +4,11 @@ import { useNavigate, useLocation } from 'react-router-dom';
 
 function SingleIssue() {
   const location = useLocation();
-  const { key, issueId } = location.state; // useLocation을 통해 전달된 state에서 key와 issueId 추출
-  const [issueDetails, setIssueDetails] = useState(null); // 이슈 상세 정보를 저장할 상태
+  const navigate = useNavigate(); // useNavigate 훅을 사용하여 navigate 함수 가져오기
+  const { key, issueId } = location.state;
+  const [issueDetails, setIssueDetails] = useState(null);
   const [error, setError] = useState('');
+  const projectKey = location.state?.key; 
 
   const fetchIssueDetails = async () => {
     try {
@@ -15,19 +17,39 @@ function SingleIssue() {
           'Content-Type': 'application/json'
         }
       });
-      // 여기를 수정하여, issueDetails에 response.data.result를 저장합니다.
-      setIssueDetails(response.data.result); // 서버로부터 받은 데이터의 result를 상태에 저장
+      setIssueDetails(response.data.result);
     } catch (error) {
       console.error('이슈 상세 정보를 가져오는 데 실패했습니다:', error);
       setError('이슈 상세 정보를 가져오는 데 실패했습니다.');
     }
+  };
+
+  const handleDeleteIssue = async () => {
+    const isConfirmed = window.confirm('정말로 삭제하시겠습니까?');
+    if (isConfirmed) {
+      try {
+        await axios.delete(`/api/projects/${key}/issues/${issueId}`, {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+        // 삭제 후 이슈 목록 페이지로 이동하면서 projectKey를 state로 전달합니다.
+        // 이 예제에서는 /issues 경로를 이슈 목록 페이지로 가정합니다.
+        // 실제 애플리케이션의 이슈 목록 페이지 경로에 맞게 수정해야 합니다.
+        navigate('/issues', { state: { projectKey: key } });
+      } catch (error) {
+        console.error('이슈 삭제에 실패했습니다:', error);
+        setError('이슈 삭제에 실패했습니다.');
+      }
+    }
   };  
+  
 
   useEffect(() => {
-    if (key && issueId) { // key와 issueId가 유효한 경우에만 요청을 보냅니다.
+    if (key && issueId) {
       fetchIssueDetails();
     }
-  }, [key, issueId]); // 컴포넌트 마운트 시 이슈 상세 정보 요청
+  }, [key, issueId]);
 
   if (!issueDetails || !issueDetails.issue) {
     return <div>Loading...</div>;
@@ -36,7 +58,6 @@ function SingleIssue() {
   return (
     <div className="container">
       <h1>이슈 상세</h1>
-      <p></p>
       {error && <p style={{ color: 'red' }}>{error}</p>}
       <div>
         <p>{issueDetails.issue.title}</p>
@@ -46,7 +67,8 @@ function SingleIssue() {
         <p>시작일: {issueDetails.issue.startDate}</p>
         <p>종료일: {issueDetails.issue.endDate}</p>
         <p>내용: {issueDetails.issue.content.text}</p>
-        {/* 여기에 더 많은 상세 정보를 표시할 수 있습니다. */}
+        <button>이슈 수정</button>
+        <button onClick={handleDeleteIssue}>이슈 삭제</button>
       </div>
     </div>
   );
