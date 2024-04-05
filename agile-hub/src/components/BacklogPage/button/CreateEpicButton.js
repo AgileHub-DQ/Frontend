75cd387974dfceb0618e24ca6f0ab3f4aed54d64
ju wonhee <jwonhee398@gmail.com>
2024-04-06 06:17:59 +0900

    feat:15 배치 & 색상 완료

--- agile-hub/src/components/BacklogPage/button/CreateEpicButton.js
+++ agile-hub/src/components/BacklogPage/button/CreateEpicButton.js
@@ -1,31 +1,18 @@
 import React, { useState } from 'react';
-import '../../../css/BacklogPage/CreateEpicButton.css';
-import EpicModal from '../modal/EpicModal.js';
-import ShowEpic from '../show/ShowEpic.js';
+import Issue from '../../../pages/Issue.js'; // Issue 컴포넌트 경로에 맞게 수정
 
 function CreateEpicButton() {
-    const [showModal, setShowModal] = useState(false);
-    const [epicList, setEpicList] = useState([]);
+    const [showIssue, setShowIssue] = useState(false); // 이슈 컴포넌트 표시 여부 상태 추가
 
-    const handleToggleModal = () => {
-        setShowModal(!showModal);
-    };
-
-    const handleEpicSubmit = (newEpicInfo) => {
-        // 새로운 에픽 정보를 에픽 목록에 추가
-        setEpicList([...epicList, newEpicInfo]);
-        // 모달 닫기
-        setShowModal(false);
+    const handleToggleIssue = () => {
+        setShowIssue(!showIssue);
     };
 
     return (
         <div className='addEpic'>
-            {showModal && <EpicModal onClose={handleToggleModal} onSubmit={handleEpicSubmit} />} 
-            {epicList.map((epicInfo, index) => (
-                <ShowEpic key={index} epicInfo={epicInfo} />
-            ))}
+            {showIssue && <Issue />} {/* 이슈 컴포넌트 표시 */}
             <div className='epic_button_container'>
-                <button className="epic_button" onClick={handleToggleModal}>
+                <button className="epic_button" onClick={handleToggleIssue}>
                     <span className="epic_button__text">에픽 생성하기</span>
                 </button>
             </div>
--- agile-hub/src/pages/CreateProject.js
+++ agile-hub/src/pages/CreateProject.js
@@ -1,12 +1,24 @@
 // CreateProject.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import '../css/CreateProject.css';
 
 function CreateProject() {
   const [projectName, setProjectName] = useState('');
   const [projectKey, setProjectKey] = useState('');
   const [error, setError] = useState('');
+  const [isProjectNameEntered, setIsProjectNameEntered] = useState(false);
+  const [isProjectKeyEntered, setIsProjectKeyEntered] = useState(false);
 
+  useEffect(() => {
+    setIsProjectNameEntered(!!projectName);
+  }, [projectName]);
+
+  useEffect(() => {
+    setIsProjectKeyEntered(!!projectKey);
+  }, [projectKey]);
+
+  
   const handleSubmit = async (event) => {
     event.preventDefault();
   
@@ -33,27 +45,40 @@ function CreateProject() {
 
   return (
     <div className="container">
+      {/* <div className='group183'/> */}
+      <div className={`text1 ${isProjectNameEntered ? 'blue-text' : 'grey-text'}`}>프로젝트 이름</div>
+      <div className={`text2 ${isProjectKeyEntered ? 'blue-text' : 'grey-text'}`}>프로젝트 키</div>
       <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="projectName">프로젝트 이름:</label>
+          {/* <label htmlFor="projectName">프로젝트 이름:</label> */}
           <input
             id="projectName"
             type="text"
             value={projectName}
+            placeholder='프로젝트 이름'
+            className='projectName'
             onChange={(e) => setProjectName(e.target.value)}
           />
         </div>
         <div>
-          <label htmlFor="projectKey">프로젝트 키:</label>
+          {/* <label htmlFor="projectKey">프로젝트 키:</label> */}
           <input
             id="projectKey"
             type="text"
             value={projectKey}
+            placeholder='프로젝트 키'
+            className='projectKey'
             onChange={(e) => setProjectKey(e.target.value)}
           />
         </div>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-        <button type="submit">프로젝트 생성</button>
+        {error && <p className='error'>{error}</p>}
+        <button type='submit' className='button' >프로젝트 생성
+        <svg width="34" height="34" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="37" cy="37" r="35.5" stroke="black" stroke-width="3"></circle>
+        <path d="M25 35.5C24.1716 35.5 23.5 36.1716 23.5 37C23.5 37.8284 24.1716 38.5 25 38.5V35.5ZM49.0607 38.0607C49.6464 37.4749 49.6464 36.5251 49.0607 35.9393L39.5147 26.3934C38.9289 25.8076 37.9792 25.8076 37.3934 26.3934C36.8076 26.9792 36.8076 27.9289 37.3934 28.5147L45.8787 37L37.3934 45.4853C36.8076 46.0711 36.8076 47.0208 37.3934 47.6066C37.9792 48.1924 38.9289 48.1924 39.5147 47.6066L49.0607 38.0607ZM25 38.5L48 38.5V35.5L25 35.5V38.5Z" fill="black"></path>
+    </svg>
+    </button>
+        {/* <button type="submit" className='button'>프로젝트 생성</button> */}
       </form>
     </div>
   );
