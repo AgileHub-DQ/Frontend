24e01553f58f807958a9f36fd95dea4e6ca0948e
ju wonhee <jwonhee398@gmail.com>
2024-04-13 23:16:43 +0900

    feat:17 상단바 css 및 위치 배치 완료

--- agile-hub/src/components/TimelinePage/Component_t.js
+++ agile-hub/src/components/TimelinePage/Component_t.js
@@ -1,7 +1,21 @@
 // Component.js
+import { useState } from 'react';
 import '../../css/TimeLinePage/Component_t.css';
-import { FaSearch } from 'react-icons/fa'; // FaSearch 아이콘을 import
+import { FaSearch } from 'react-icons/fa';
 function Component_t() {
+  // const [showOptions, setShowOptions] = useState(false); 
+
+
+  // const toggleOptions = () => {
+  //   setShowOptions(!showOptions);
+  // };
+
+
+  // const handleOptionChange = (e) => {
+
+  //   console.log('Checkbox changed:', e.target.name, e.target.checked);
+  // };
+
 
   return (
     <div className='bar'> 
@@ -10,24 +24,36 @@ function Component_t() {
       <div className='timeline_text'>타임라인</div>
     </div>
       <div className='bar2container'>    
-    <div className="search-container">
-        <input type="text" id="search" className="search" placeholder="검색..." />
-        <FaSearch className="search-icon" />
-    </div>
-    <select id="status" className='status'>
-    <option value="default">상태 범주</option>
-      <option value="preparing">Preparing</option>
-      <option value="in-progress">In Progress</option>
-      <option value="complete">Complete</option>
-    </select>
-    <select id="type" className='type'>
+        <div className="search-container">
+          <input type="text" id="search" className="search" placeholder="검색..." />
+          <FaSearch className="search-icon" />
+        </div>
+        <div className='status-container'>
+        <div className="status-label">상태 범주</div>
+          {/* {showOptions && (
+            <fieldset className='fieldset' onClick={handleOptionChange}>
+              <div>
+                <input type="checkbox" id="preparing" name="preparing"/>
+                <label htmlFor="preparing">Preparing</label>
+              </div>
+              <div>
+                <input type="checkbox" id="in-progress" name="in-progress" />
+                <label htmlFor="in-progress">In Progress</label>
+              </div>
+              <div>
+                <input type="checkbox" id="complete" name="complete" />
+                <label htmlFor="complete">Complete</label>
+              </div>
+            </fieldset> */}
+        </div>
+      <div className="type-label">에픽</div>
+    {/* <select id="type" className='type'>
     <option value="default">타입</option>
         <option value="epic">Epic</option>
         <option value="story">Story</option>
         <option value="task">Task</option>
-    </select>
+    </select> */}
     </div>
-
     </div>
   );
 }
