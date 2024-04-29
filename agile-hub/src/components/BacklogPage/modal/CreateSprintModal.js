e677521aacb8c31d81d261924444d74e639d7618
drimh <107299318+drimh@users.noreply.github.com>
2024-04-29 13:09:31 +0900

    feat: createSprintModal api 수정

--- agile-hub/src/components/BacklogPage/modal/CreateSprintModal.js
+++ agile-hub/src/components/BacklogPage/modal/CreateSprintModal.js
@@ -30,12 +30,15 @@ function CreateSprintModal() {
         formData.append('startDate', startDate);
         formData.append('endDate', endDate);
         formData.append('description', description);
+
+        const accessToken = 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJBZ2lsZUh1YiIsInN1YiI6IkFjY2Vzc1Rva2VuIiwibmFtZSI6IuyLoOyKue2YnCIsInJvbGUiOiJST0xFX1VTRVIiLCJwcm92aWRlciI6Imtha2FvIiwiZGlzdGluY3RJZCI6IjM0NTcyMjMzOTYiLCJpYXQiOjE3MTQyODMzNTYsImV4cCI6MTcxNTQ5Mjk1Nn0.PGInkoWYOAY_GsY_vO462E0dOcn-yHvlqPaa6P4SSttUtj7fW48q9DvkjSuT1I-VUxmZ04knuVK6JIZffVzyXg'; 
     
         try {
-          const endpoint = `/api/projects/${projectKey}/sprints`;
+          const endpoint = `/projects/${projectKey}/sprints`;
           const response = await axios.post(endpoint, formData, {
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json', 
+                'Authorization': `Bearer ${accessToken}`
             }
           });
           console.log(response.data.result);
