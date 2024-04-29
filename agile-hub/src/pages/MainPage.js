8626a17e0069b4f6396a8092bd22654f2bf25f8e
ju wonhee <jwonhee398@gmail.com>
2024-04-29 13:10:42 +0900

    feat:16 mainpage 오류 해결

--- agile-hub/src/pages/MainPage.js
+++ agile-hub/src/pages/MainPage.js
@@ -1,40 +1,3 @@
-// // MainPage.js
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
-
-// function MainPage() {
-//   const [responseData, setResponseData] = useState(null);
-//   const [error, setError] = useState(null);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await axios.get("/api/health");
-//         setResponseData("API Response:" + response.data); 
-//       } catch (error) {
-//         console.error('fetch error', error);
-//         setError('API 요청에 문제 발생');
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   return (
-//     <>
-//       <h1>여기는 MainPage입니다.</h1>
-//       {error && <p>Error: {error}</p>}
-//       {responseData && (
-//         <div>
-//           <h2>API Response: {responseData}</h2>
-//         </div>
-//       )}
-//     </>
-//   );
-// }
-
-// export default MainPage;
-// MainPage.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
@@ -53,7 +16,8 @@ function MainPage() {
           }
         });
 
-        setResponseData(response.data);
+        setResponseData(response.statusText);
+        
       } catch (error) {
         console.error('fetch error', error);
         setError('API 요청에 문제 발생');
@@ -67,6 +31,7 @@ function MainPage() {
     <>
       <h1>여기는 MainPage입니다.</h1>
       {error && <p>Error: {error}</p>}
+      
       {responseData && (
         <div>
           <h2>API Response: {responseData}</h2>
