119c7f24865d836effce3ea39dd9323d45bf7e1a
ju wonhee <jwonhee398@gmail.com>
2024-04-29 12:47:40 +0900

    feat:16

--- agile-hub/src/setupProxy.js
+++ agile-hub/src/setupProxy.js
@@ -4,9 +4,8 @@ module.exports = function(app) {
   app.use(
     '/api', //proxy가 필요한 path parameter
     createProxyMiddleware({
-      target: 'https://api.agilehub.store', //타겟이 되는 api url
+      target: 'https://api.agilehub.store/', //타겟이 되는 api url
       changeOrigin: true, // 서버 구성에 따른 호스트 헤더 변경 여부 설정
     })
   );
 };
-
