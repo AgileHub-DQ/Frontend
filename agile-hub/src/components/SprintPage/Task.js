dd581a8d4fcd0bcd8a3ace1df925339fa4e9d4aa
ju wonhee <jwonhee398@gmail.com>
2024-04-29 16:49:44 +0900

    feat:16 projectKey 클릭 시 storymodal창으로 조회

--- agile-hub/src/components/SprintPage/Task.js
+++ agile-hub/src/components/SprintPage/Task.js
@@ -2,19 +2,10 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import '../../css/SprintPage/Task.css';
 
-function Task({ text, draggable, onDragStart, onDragEnd, projectKey, issue }) {
+function Task({ projectKey, issue }) {
   const [response, setResponse] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
-
-  const handleDragStart = (e) => {
-    e.dataTransfer.setData('text/plain', text); 
-    onDragStart(); 
-  };
-
-  const handleDragEnd = () => {
-    onDragEnd(); 
-  };
 
   const fetchIssues = async () => {
     try {
@@ -48,11 +39,15 @@ function Task({ text, draggable, onDragStart, onDragEnd, projectKey, issue }) {
     return <div>Error: 이슈를 불러올 수 없습니다.</div>;
   }
 
+  const handleIssueTitleClick = () =>{
+    //const issueDetail = `/projects/${projectKey}/issues/${issue.id}`;
+  }
   return (
-    <div className="task" draggable={draggable} onDragStart={handleDragStart} onDragEnd={handleDragEnd}>
+    <div className="task">
       <div className='typecolor'></div> {/* 단계의 값으로 설정 */}
-      <div className='issueTitle'> {issue.title} </div>
+      <div className='issueTitle' onClick={handleIssueTitleClick}> {issue.title} </div>
       <div className='issueType'> {issue.type} </div>
+      <div className='issueAssigneeName'> 담당자: {response.result.issue.assignee.name} </div>
       {/* <div className='issueAssigneeName'> 담당자: {issue.assignee.name} </div> */}
       
       {/* <div className='issueTitle'> {response.result.issue.title} </div>
