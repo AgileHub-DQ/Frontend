6eab192f55e7c388b53d942a00f7fd43ed949dd4
ju wonhee <jwonhee398@gmail.com>
2024-04-30 23:19:52 +0900

    feat:16 backlog page 에픽 모달 창 위치 지정

--- agile-hub/src/components/BacklogPage/button/CreateEpicButton.js
+++ agile-hub/src/components/BacklogPage/button/CreateEpicButton.js
@@ -1,57 +1,32 @@
-// import React, { useState } from 'react';
-// import '../../../css/BacklogPage/CreateEpicButton.css';
-// import EpicModal from '../modal/EpicModal.js';
-// import ShowEpic from '../show/ShowEpic.js';
-
-// function CreateEpicButton() {
-//     const [showModal, setShowModal] = useState(false);
-//     const [epicList, setEpicList] = useState([]);
-
-//     const handleToggleModal = () => {
-//         setShowModal(!showModal);
-//     };
-
-//     const handleEpicSubmit = (newEpicInfo) => {
-//         // 새로운 에픽 정보를 에픽 목록에 추가
-//         setEpicList([...epicList, newEpicInfo]);
-//         // 모달 닫기
-//         setShowModal(false);
-//     };
-
-//     return (
-//         <div className='addEpic'>
-
-//             {showModal && <EpicModal onClose={handleToggleModal} onSubmit={handleEpicSubmit} />} 
-//             {epicList.map((epicInfo, index) => (
-//                 <ShowEpic key={index} epicInfo={epicInfo} />
-//             ))}
-//             <div className='epic_button_container'>
-//                 <button className="epic_button" onClick={handleToggleModal}>
-//                     <span className="epic_button__text">에픽 생성하기</span>
-//                 </button>
-//             </div>
-//         </div>
-//     );
-// }
-
-// export default CreateEpicButton;
 import React, { useState } from 'react';
 import '../../../css/BacklogPage/CreateEpicButton.css';
 import EpicModal from '../modal/EpicModal.js';
-// import Issue from '../../../pages/Issue.js'; // Issue 컴포넌트 경로에 맞게 수정
+import ShowEpic from '../show/ShowEpic.js';
 
 function CreateEpicButton() {
-    const [showIssue, setShowIssue] = useState(false); // 이슈 컴포넌트 표시 여부 상태 추가
+    const [showModal, setShowModal] = useState(false);
+    const [epicList, setEpicList] = useState([]);
 
-    const handleToggleIssue = () => {
-        setShowIssue(!showIssue);
+    const handleToggleModal = () => {
+        setShowModal(!showModal);
+    };
+
+    const handleEpicSubmit = (newEpicInfo) => {
+        // 새로운 에픽 정보를 에픽 목록에 추가
+        setEpicList([...epicList, newEpicInfo]);
+        // 모달 닫기
+        setShowModal(false);
     };
 
     return (
         <div className='addEpic'>
-            {showIssue && <EpicModal onClose={handleToggleIssue} />} {/* 이슈 컴포넌트 표시 */}
+
+            {showModal && <EpicModal onClose={handleToggleModal} onSubmit={handleEpicSubmit} />} 
+            {epicList.map((epicInfo, index) => (
+                <ShowEpic key={index} epicInfo={epicInfo} />
+            ))}
             <div className='epic_button_container'>
-                <button className="epic_button" onClick={handleToggleIssue}>
+                <button className="epic_button" onClick={handleToggleModal}>
                     <span className="epic_button__text">에픽 생성하기</span>
                 </button>
             </div>
@@ -60,3 +35,30 @@ function CreateEpicButton() {
 }
 
 export default CreateEpicButton;
+
+
+// import React, { useState } from 'react';
+// import '../../../css/BacklogPage/CreateEpicButton.css';
+// import EpicModal from '../modal/EpicModal.js';
+// // import Issue from '../../../pages/Issue.js'; // Issue 컴포넌트 경로에 맞게 수정
+
+// function CreateEpicButton() {
+//     const [showIssue, setShowIssue] = useState(false); // 이슈 컴포넌트 표시 여부 상태 추가
+
+//     const handleToggleIssue = () => {
+//         setShowIssue(!showIssue);
+//     };
+
+//     return (
+//         <div className='addEpic'>
+//             {showIssue && <EpicModal onClose={handleToggleIssue} />} {/* 이슈 컴포넌트 표시 */}
+//             <div className='epic_button_container'>
+//                 <button className="epic_button" onClick={handleToggleIssue}>
+//                     <span className="epic_button__text">에픽 생성하기</span>
+//                 </button>
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default CreateEpicButton;
--- agile-hub/src/components/BacklogPage/modal/EpicModal.js
+++ agile-hub/src/components/BacklogPage/modal/EpicModal.js
@@ -1,224 +1,253 @@
+// // EpicModal.js
+// import React, { useState } from 'react';
+// import axios from 'axios';
+// import '../../../css/modal/EpicModal.css'; 
+
+// function EpicModal({ onClose, onSubmit }) {
+//     const [issueTitle, setIssueTitle] = useState('');
+//     const [type, setType] = useState('EPIC'); 
+//     const [status, setStatus] = useState('DO');
+//     const [content, setContent] = useState('');
+//     const [files, setFiles] = useState(''); 
+//     const [imageURLInput, setImageURLInput] = useState('');
+//     const [startDate, setStartDate] = useState('');
+//     const [endDate, setEndDate] = useState('');
+//     const [assigneeId, setAssigneeId] = useState('1');
+//     const [parentId, setParentId] = useState('1');
+//     const [color, setColor] = useState('#FF7041'); 
+
+//     // const handleChange = (e) => {
+//     //     const { name, value } = e.target;
+//     //     setEpicInfo(prevState => ({
+//     //         ...prevState,
+//     //         [name]: value
+//     //     }));
+//     // };
+
+//     // const handleSubmit = (e) => {
+//     //     e.preventDefault();
+//     //     onSubmit(epicInfo); // 입력된 값 전달
+//     //     onClose(); // 모달 닫기
+//     // };
+
+//     const handleSubmit = async (e) => {
+//         e.preventDefault();
+    
+//         const epicInfo = new FormData();
+//         epicInfo.append('title', issueTitle);
+//         epicInfo.append('type', type); // 상위에픽이 무엇인지에 대한 코드로 수정되어야 함
+//         epicInfo.append('status', status);
+//         epicInfo.append('content', content);
+//         epicInfo.append('startDate', startDate);
+//         epicInfo.append('endDate', endDate);
+//         epicInfo.append('assigneeId', assigneeId);
+//         epicInfo.append('parentId', parentId);
+//         // epicInfo.append('sprintId',sprintId);
+    
+//         if (files.length > 0) {
+//           for (let i = 0; i < files.length; i++) {
+//             epicInfo.append('files', files[i]);
+//           }
+//         }
+    
+//         try {
+//         const projectKey = 'P1';
+//           const accessToken = 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJBZ2lsZUh1YiIsInN1YiI6IkFjY2Vzc1Rva2VuIiwibmFtZSI6IuyLoOyKue2YnCIsInJvbGUiOiJST0xFX1VTRVIiLCJwcm92aWRlciI6Imtha2FvIiwiZGlzdGluY3RJZCI6IjM0NTcyMjMzOTYiLCJpYXQiOjE3MTQyODMzNTYsImV4cCI6MTcxNTQ5Mjk1Nn0.PGInkoWYOAY_GsY_vO462E0dOcn-yHvlqPaa6P4SSttUtj7fW48q9DvkjSuT1I-VUxmZ04knuVK6JIZffVzyXg';
+//           const endpoint = `/projects/${projectKey}/issues`;
+//           console.log("endpoint:"+endpoint);
+//           const response = await axios.post(endpoint, epicInfo, {
+//             headers: {
+//               Authorization: `Bearer ${accessToken}`,
+//               'Content-Type': 'multipart/form-data'
+//             }
+//           });
+    
+    
+//           console.log(response.data);
+//           setIssueTitle('');
+//           setType('EPIC');
+//           setStatus('DO');
+//           setContent('');
+//           setFiles('');
+//           setStartDate('');
+//           setEndDate('');
+//           setAssigneeId('2');
+//           setParentId('1');
+//           onClose();
+          
+//           // 폼 입력을 마치면 폼은 초기화 되고 폼은 닫힘
+        
+//         } catch (error) {
+//           console.error('떼잉~~ 실패!!', error);
+//           console.log(error.response)
+//         }
+//       };
+
+//     // const handleCloseModal = () => {
+//     //     onClose(); // onClose prop으로 전달된 함수를 호출하여 모달 닫기
+//     // };
+
+//     const handleFileChange = (e) => {
+//         setFiles(e.target.files); 
+//       };
+    
+//       const handleTypeChange = (e) => {
+//         const selectedType = e.target.value;
+//         setType(selectedType);
+//         // 선택한 값에 따라 색상 변경
+//         switch (selectedType) {
+//           case 'EPIC':
+//             setColor('#FF7041');
+//             break;
+//           case 'STORY':
+//             setColor('#00FF75');
+//             break;
+//           case 'TASK':
+//             setColor('#FB55B3');
+//             break;
+//           default:
+//             setColor('#95ADF6');
+//         }
+//       };
+
+//     return (
+//         <div className='modal-container'>
+//         <div className="modal-content">
+
+//                 <form onSubmit={handleSubmit}>
+
+//                     <div className='form-row'>
+//     <div className='colorBox' style={{backgroundColor: color}}></div>
+//     <input
+//         type="text"
+//         className="form-input"
+//         placeholder="Issue Title"
+//         value={issueTitle}
+//         onChange={(e) => setIssueTitle(e.target.value)}
+//     />
+//         <select
+//       className="form-select-status"
+//       value={status}
+//       onChange={(e) => setStatus(e.target.value)}
+//     >
+//       <option value="DO">DO</option>
+//       <option value="PROGRESS">PROGRESS</option>
+//       <option value="DONE">DONE</option>
+//     </select>
+// </div>
+// <div className='form-row-2'>
+//     <p className="form-label">기간</p>
+//     <input
+//       type="date"
+//       className="form-date"
+//       value={startDate}
+//       onChange={(e) => setStartDate(e.target.value)}
+//     />
+//     <p>~</p>
+//     <input
+//       type="date"
+//       className="form-date"
+//       value={endDate}
+//       min={startDate}
+//       onChange={(e) => setEndDate(e.target.value)}
+//     />
+// </div>
+//     <p className="form-label-tag">단계</p>
+// <div className='form-row-3'>
+//     <div className='box1'>계획</div>
+//     <div className='box2'>디자인</div>
+//     <div className='box3'>개발</div>
+//     <div className='box4'>테스트</div>
+//     <div className='box5'>피드백</div>
+// </div>
+// <div className='form-row-4'>
+//     <p className="form-label">타입</p>
+//     <select
+//       className="form-select-type"
+//       value={type}
+//       onChange={handleTypeChange}
+//     >
+//        <option value="EPIC">EPIC</option>
+
+//     </select>
+// </div>
+
+
+//     <p className="form-label-d">설명</p>
+//     <textarea
+//       className="form-textarea"
+//       placeholder="설명 입력"
+//       value={content}
+//       onChange={(e) => setContent(e.target.value)}
+//     />
+// <div className='form-row-5'>
+//     <p className="form-label">이미지 파일</p>
+//     <input
+//       type="file"
+//       className="form-file"
+//       multiple
+//       onChange={handleFileChange}
+//     />
+
+// </div>
+
+//   <div className='form-row-7'>
+//     <button className="form-button" type="submit">이슈 생성</button>
+//   </div> 
+// </form>
+//         </div>
+//         </div>
+//     );
+// }
+
+// export default EpicModal;
+
 // EpicModal.js
 import React, { useState } from 'react';
-import axios from 'axios';
 import '../../../css/modal/EpicModal.css'; 
 
 function EpicModal({ onClose, onSubmit }) {
-    const [issueTitle, setIssueTitle] = useState('');
-    const [type, setType] = useState('EPIC'); 
-    const [status, setStatus] = useState('DO');
-    const [content, setContent] = useState('');
-    const [files, setFiles] = useState(''); 
-    const [imageURLInput, setImageURLInput] = useState('');
-    const [startDate, setStartDate] = useState('');
-    const [endDate, setEndDate] = useState('');
-    const [assigneeId, setAssigneeId] = useState('1');
-    const [parentId, setParentId] = useState('1');
-    const [color, setColor] = useState('#FF7041'); 
+    const [epicInfo, setEpicInfo] = useState({
+        info1: '',
+        info2: ''
+    });
 
-    // const handleChange = (e) => {
-    //     const { name, value } = e.target;
-    //     setEpicInfo(prevState => ({
-    //         ...prevState,
-    //         [name]: value
-    //     }));
-    // };
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setEpicInfo(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
 
-    // const handleSubmit = (e) => {
-    //     e.preventDefault();
-    //     onSubmit(epicInfo); // 입력된 값 전달
-    //     onClose(); // 모달 닫기
-    // };
-
-    const handleSubmit = async (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
-    
-        const epicInfo = new FormData();
-        epicInfo.append('title', issueTitle);
-        epicInfo.append('type', type); // 상위에픽이 무엇인지에 대한 코드로 수정되어야 함
-        epicInfo.append('status', status);
-        epicInfo.append('content', content);
-        epicInfo.append('startDate', startDate);
-        epicInfo.append('endDate', endDate);
-        epicInfo.append('assigneeId', assigneeId);
-        epicInfo.append('parentId', parentId);
-        // epicInfo.append('sprintId',sprintId);
-    
-        if (files.length > 0) {
-          for (let i = 0; i < files.length; i++) {
-            epicInfo.append('files', files[i]);
-          }
-        }
-    
-        try {
-        const projectKey = 'P1';
-          const accessToken = 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJBZ2lsZUh1YiIsInN1YiI6IkFjY2Vzc1Rva2VuIiwibmFtZSI6IuyLoOyKue2YnCIsInJvbGUiOiJST0xFX1VTRVIiLCJwcm92aWRlciI6Imtha2FvIiwiZGlzdGluY3RJZCI6IjM0NTcyMjMzOTYiLCJpYXQiOjE3MTQyODMzNTYsImV4cCI6MTcxNTQ5Mjk1Nn0.PGInkoWYOAY_GsY_vO462E0dOcn-yHvlqPaa6P4SSttUtj7fW48q9DvkjSuT1I-VUxmZ04knuVK6JIZffVzyXg';
-          const endpoint = `/projects/${projectKey}/issues`;
-          console.log("endpoint:"+endpoint);
-          const response = await axios.post(endpoint, epicInfo, {
-            headers: {
-              Authorization: `Bearer ${accessToken}`,
-              'Content-Type': 'multipart/form-data'
-            }
-          });
-    
-    
-          console.log(response.data);
-          setIssueTitle('');
-          setType('EPIC');
-          setStatus('DO');
-          setContent('');
-          setFiles('');
-          setStartDate('');
-          setEndDate('');
-          setAssigneeId('2');
-          setParentId('1');
-          onClose();
-          
-          // 폼 입력을 마치면 폼은 초기화 되고 폼은 닫힘
-        
-        } catch (error) {
-          console.error('떼잉~~ 실패!!', error);
-          console.log(error.response)
-        }
-      };
+        onSubmit(epicInfo); // 입력된 값 전달
+        onClose(); // 모달 닫기
+    };
 
-    // const handleCloseModal = () => {
-    //     onClose(); // onClose prop으로 전달된 함수를 호출하여 모달 닫기
-    // };
-
-    const handleFileChange = (e) => {
-        setFiles(e.target.files); 
-      };
-    
-      const handleTypeChange = (e) => {
-        const selectedType = e.target.value;
-        setType(selectedType);
-        // 선택한 값에 따라 색상 변경
-        switch (selectedType) {
-          case 'EPIC':
-            setColor('#FF7041');
-            break;
-          case 'STORY':
-            setColor('#00FF75');
-            break;
-          case 'TASK':
-            setColor('#FB55B3');
-            break;
-          default:
-            setColor('#95ADF6');
-        }
-      };
+    const handleCloseModal = () => {
+        onClose(); // onClose prop으로 전달된 함수를 호출하여 모달 닫기
+    };
 
     return (
-        <div className='modal-container'>
         <div className="modal-content">
-            {/* <div className="modal-header">
+            <div className="modal-header">
                 <span className="close" onClick={handleCloseModal}>&times;</span>
                 <h2>에픽 생성 모달창</h2>
-            </div> */}
-            {/* <div className="modal-body"> */}
+            </div>
+            <div className="modal-body">
                 <form onSubmit={handleSubmit}>
-                    {/* <div>
+                    <div>
                         <label htmlFor="info1">에픽 이름: </label>
                         <input type="text" id="info1" name="info1" value={epicInfo.info1} onChange={handleChange} />
-                    </div> */}
-                    <div className='form-row'>
-    <div className='colorBox' style={{backgroundColor: color}}></div>
-    <input
-        type="text"
-        className="form-input"
-        placeholder="Issue Title"
-        value={issueTitle}
-        onChange={(e) => setIssueTitle(e.target.value)}
-    />
-        <select
-      className="form-select-status"
-      value={status}
-      onChange={(e) => setStatus(e.target.value)}
-    >
-      <option value="DO">DO</option>
-      <option value="PROGRESS">PROGRESS</option>
-      <option value="DONE">DONE</option>
-    </select>
-</div>
-<div className='form-row-2'>
-    <p className="form-label">기간</p>
-    <input
-      type="date"
-      className="form-date"
-      value={startDate}
-      onChange={(e) => setStartDate(e.target.value)}
-    />
-    <p>~</p>
-    <input
-      type="date"
-      className="form-date"
-      value={endDate}
-      min={startDate}
-      onChange={(e) => setEndDate(e.target.value)}
-    />
-</div>
-    <p className="form-label-tag">단계</p>
-<div className='form-row-3'>
-    <div className='box1'>계획</div>
-    <div className='box2'>디자인</div>
-    <div className='box3'>개발</div>
-    <div className='box4'>테스트</div>
-    <div className='box5'>피드백</div>
-</div>
-<div className='form-row-4'>
-    <p className="form-label">타입</p>
-    {/* 에픽 목록 보여주는 코드로 변경되어야함 */}
-    <select
-      className="form-select-type"
-      value={type}
-      onChange={handleTypeChange}
-    >
-       <option value="EPIC">EPIC</option>
-      {/* <option value="STORY">STORY</option>
-      <option value="TASK">TASK</option> */}
-    </select>
-</div>
-{/* <div className='form-row-10'>
-    <p className="form-label">상위 항목</p>
-    <select
-      className="form-select-type">
-      <option>EPIC1</option>
-      <option>EPIC2</option>
-    </select>
-</div> */}
-
-    <p className="form-label-d">설명</p>
-    <textarea
-      className="form-textarea"
-      placeholder="설명 입력"
-      value={content}
-      onChange={(e) => setContent(e.target.value)}
-    />
-<div className='form-row-5'>
-    <p className="form-label">이미지 파일</p>
-    <input
-      type="file"
-      className="form-file"
-      multiple
-      onChange={handleFileChange}
-    />
-    {/* 파일 삭제 코드 있어야 함 */}
-</div>
-{/* <div className='form-row-6'>
-  <button className='addUnderIssue'>하위 이슈 추가</button>
-</div> */}
-  <div className='form-row-7'>
-    <button className="form-button" type="submit">이슈 생성</button>
-  </div> 
-                    {/* <button type="submit">완료</button> */}
+                    </div>
+                    <button type="submit">완료</button>
                 </form>
-            {/* </div> */}
-            {/* <div className="modal-footer">
+            </div>
+            <div className="modal-footer">
                 <h3>footer</h3>
-            </div> */}
-        </div>
+            </div>
         </div>
     );
 }
