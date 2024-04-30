01f874a2fc2d1ee1161b5b0a196ab2c24687f86c
ju wonhee <jwonhee398@gmail.com>
2024-04-30 15:57:51 +0900

    feat:16 이슈 조회창 닫기 버튼 구현

--- agile-hub/src/components/SprintPage/StoryModal.js
+++ agile-hub/src/components/SprintPage/StoryModal.js
@@ -2,12 +2,38 @@ import React, { useState } from 'react';
 import '../../css/modal/Modal.css'; // 모달 스타일을 위한 CSS 파일
 
 const Modal = ({ isVisible, details, onClose }) => {
+
   if (!isVisible) return null;
+
+
 
   return(
 <div className="modalContainer">
+<div className='폼로우'>
+    {/* <div className='colorBox' style={{backgroundColor: color}}></div> */}
+    {/* <input
+        type="text"
+        className="form-input"
+        placeholder="Issue Title"
+        value={issueTitle}
+        onChange={(e) => setIssueTitle(e.target.value)}
+    /> */}
+    <div className='issueTitle'>{details.result.issue.title}</div>
+    <select className='issueStatus' defaultValue={details.result.issue.status}>
+        <option value="DO">DO</option>
+        <option value="PROGRESS">PROGRESS</option>
+        <option value="DONE">DONE</option>
+    </select>
+    <div>{details.result.issue.type}</div>
+    <div>{details.result.issue.startDate}</div>
+    <div>{details.result.issue.endDate}</div>
+    <div>{details.result.issue.assignee.name}</div>
+    <div>...</div>
+    <button onClick={onClose}>닫기</button>
+    <button>수정하기</button>
 
-<div>{details.result.issue.title}</div>
+</div>
+{/* <div>{details.result.issue.title}</div> */}
 
 
 </div>
