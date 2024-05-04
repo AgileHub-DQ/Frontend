95337b945b44557fa3b465c6add403a88917d863
ju wonhee <jwonhee398@gmail.com>
2024-05-05 03:15:41 +0900

    feat:16

--- agile-hub/src/components/SprintPage/DashBoard.js
+++ agile-hub/src/components/SprintPage/DashBoard.js
@@ -83,15 +83,7 @@ export default function DashBoard({ projectKey, sprintId }) {
   
     try { // 해당 카테고리로 이동되면 데이터의 status 값 변경
       const accessToken = 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJBZ2lsZUh1YiIsInN1YiI6IkFjY2Vzc1Rva2VuIiwibmFtZSI6IuyLoOyKue2YnCIsInJvbGUiOiJST0xFX1VTRVIiLCJwcm92aWRlciI6Imtha2FvIiwiZGlzdGluY3RJZCI6IjM0NTcyMjMzOTYiLCJpYXQiOjE3MTQyODMzNTYsImV4cCI6MTcxNTQ5Mjk1Nn0.PGInkoWYOAY_GsY_vO462E0dOcn-yHvlqPaa6P4SSttUtj7fW48q9DvkjSuT1I-VUxmZ04knuVK6JIZffVzyXg';
-      const movedItemEditStatus = await axios.put(`/projects/${projectKey}/issues/${id}`,
-      { endDate: movedItem.endDate,
-        issueId: movedItem.issueId,
-        key: movedItem.key,
-        startDate: movedItem.startDate,
-        status: getStatusFromCategory(newCategory),
-        title: movedItem.title,
-        type: movedItem.type,
-      }, {
+      const movedItemEditStatus = await axios.put(`/projects/${projectKey}/issues/${id}`, { title: movedItem.title, type: movedItem.type, status: getStatusFromCategory(newCategory) }, {
         headers: {
           Authorization: `Bearer ${accessToken}`,
           'Content-Type': 'multipart/form-data'
