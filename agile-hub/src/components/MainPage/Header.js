0a43f69fa9e7015cbe8529a76320cf8ce5b75409
drimh <107299318+drimh@users.noreply.github.com>
2024-05-08 15:41:23 +0900

    feat: 버튼 각주 처리 (demo -> 로그인 x)

--- agile-hub/src/components/MainPage/Header.js
+++ agile-hub/src/components/MainPage/Header.js
@@ -17,12 +17,12 @@ function Header() {
     return (
         <div style={headerStyle}>
             <img src={LogoImage} alt="AgileHub Logo" style={{ maxWidth: '150px' }} />
-            <button 
+            {/* <button 
                 style={{ padding: '0.5rem 1rem', backgroundColor: 'blue', color: 'white', borderRadius: '20px' }}
                 onClick={() => navigate('/login')}  // 이 부분에 클릭 이벤트 추가
             >
                 Sign In
-            </button>
+            </button> */}
         </div>
     );
 }
