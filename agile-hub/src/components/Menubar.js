bba7ba789cc09f1659a4af825be7ef61e072b090
drimh <107299318+drimh@users.noreply.github.com>
2024-05-08 17:19:41 +0900

    feat: 메뉴바 UI 수정

--- agile-hub/src/components/Menubar.js
+++ agile-hub/src/components/Menubar.js
@@ -1,11 +1,81 @@
-// Menubar.js
 import React from 'react';
-import '../css/Menubar.css';
+import { useNavigate } from "react-router-dom";
+import logoIcon from "../assets/logoWhite.png";
+import projectIcon from "../assets/ProjectIcon.png";
+import membersIcon from "../assets/MembersIcon.png";
+import backlogIcon from "../assets/BacklogIcon.png";
+import sprintIcon from "../assets/SprintIcon.png";
+import myPageIcon from "../assets/MyPageIcon.png";
+
 function Menubar() {
+  const navigate = useNavigate();
+
+  const menubarStyle = {
+    display: 'flex',
+    flexDirection: 'column', 
+    alignItems: 'center', 
+    width: '20%', 
+    minWidth: '300px', 
+    height: '100vh',
+    float: 'left',
+    backgroundColor: '#2E4BBA'
+  };
+
+  const menuItemStyle = {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row', 
+    alignItems: 'center', 
+    margin: '10px 0', 
+    marginTop: '1.5rem'
+  };
+
+  const imageStyle = {
+    width: '50px',
+    height: 'auto',
+    marginRight: '10px' 
+  };
+
+  const logoStyle = {
+    width: '15rem', 
+    height: 'auto',
+    marginBottom: '20px', 
+    marginTop: '2rem'
+  };
+
+  const textStyle = { 
+    color: 'white', 
+    fontFamily: 'Inter', 
+    fontWeight: '700', 
+    fontSize: '0.875rem', 
+    marginLeft: '1rem'
+  }
 
   return (
-    <div className='menubar'>
-      menubar
+    <div style={menubarStyle}>
+      <img src={logoIcon} alt="Logo" style={logoStyle}/>
+      <div>
+        <div style={menuItemStyle} onClick={() => navigate('/myPage')} >
+          <img src={projectIcon} alt="Projects" style={imageStyle}/>
+          <span style={textStyle}>프로젝트</span>
+        </div>
+        <div style={menuItemStyle} onClick={() => navigate('/login')} >
+          <img src={membersIcon} alt="Members" style={imageStyle}/>
+          <span style={textStyle}>멤버</span>
+        </div>
+        <div style={menuItemStyle} onClick={() => navigate('/backlog')} >
+          <img src={backlogIcon} alt="Backlog" style={imageStyle}/>
+          <span style={textStyle}>백로그</span>
+        </div>
+        <div style={menuItemStyle} onClick={() => navigate('/sprint')} >
+          <img src={sprintIcon} alt="Sprint" style={imageStyle}/>
+          <span style={textStyle}>스프린트</span>
+        </div>
+        <div style={menuItemStyle} onClick={() => navigate('/myPage')} >
+          <img src={myPageIcon} alt="My Page" style={imageStyle}/>
+          <span style={textStyle}>마이페이지</span>
+        </div>
+      </div>
     </div>
   );
 }
