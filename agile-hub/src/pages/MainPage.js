00507e435980fa5b03af4cbe402819d99c429669
drimh <107299318+drimh@users.noreply.github.com>
2024-05-11 12:14:52 +0900

    feat: mainPage 코드 수정

--- agile-hub/src/pages/MainPage.js
+++ agile-hub/src/pages/MainPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Header from '../components/MainPage/Header'; 
 import Footer from '../components/MainPage/Footer';
 import Main1 from '../components/MainPage/Main1';
@@ -7,8 +7,6 @@ import Main3 from '../components/MainPage/Main3';
 import Main4 from '../components/MainPage/Main4';
 
 function MainPage() {
-  const [responseData, setResponseData] = useState(null);
-  const [error, setError] = useState(null);
 
   return (
     <div>
