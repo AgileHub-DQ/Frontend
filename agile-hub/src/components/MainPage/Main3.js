e3c1493964f1bd84b6fd5320623b42c09c3b22eb
drimh <107299318+drimh@users.noreply.github.com>
2024-05-12 12:14:10 +0900

    feat: Main3 수정

--- agile-hub/src/components/MainPage/Main3.js
+++ agile-hub/src/components/MainPage/Main3.js
@@ -1,35 +1,35 @@
 import React from "react";
+import "../../css/MainPage/Main3.css"; 
 
 function Main3() {
     const containerStyle = {
         display: 'flex',
-        flexDirection: 'column',    // 세로 방향으로 요소들을 정렬
-        alignItems: 'center',       // 센터 정렬
-        padding: '20px',
-        // backgroundColor: '#f0f0f0',
-        height: '100%', 
-        backgroundColor: 'yellow',
+        flexDirection: 'column',    
+        alignItems: 'center',      
+        padding: '40px',
+        backgroundColor: '#ffffff',
+        height: '35vh',
     };
 
     const descriptionStyle = {
-        fontSize: '3rem',         // 설명 텍스트 폰트 사이즈
-        fontWeight: 'normal',       // 폰트 두께
-        textAlign: 'center',        // 중앙 정렬
-        marginBottom: '20px',       // 아래 컨텐츠와의 간격
-        width: '80%'                // 너비 설정
+        fontSize: '3rem',        
+        fontWeight: 'normal',    
+        textAlign: 'center',      
+        marginBottom: '20px',      
+        width: '80%'             
     };
 
     const cardsContainerStyle = {
         display: 'flex',
-        justifyContent: 'space-around', // 카드들 사이에 균등한 간격
+        justifyContent: 'space-around', 
         alignItems: 'center',
-        width: '100%'                 // 컨테이너 너비 100%
+        width: '100%'                 
     };
 
     const cardStyle = {
-        width: '30%', // 각 카드의 너비
-        backgroundColor: '#fff', // 배경 색
-        boxShadow: '0 4px 8px rgba(0,0,0,0.1)', // 그림자 효과
+        width: '30%', 
+        backgroundColor: '#fff', 
+        boxShadow: '0 4px 8px rgba(0,0,0,0.1)', 
         padding: '20px',
         display: 'flex',
         flexDirection: 'column',
@@ -37,9 +37,9 @@ function Main3() {
     };
 
     const imagePlaceholderStyle = {
-        width: '100%', // 이미지 플레이스홀더 너비
-        height: '200px', // 이미지 플레이스홀더 높이
-        backgroundColor: '#ddd', // 플레이스홀더 색
+        width: '100%',
+        height: '200px', 
+        backgroundColor: '#ddd',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
@@ -47,7 +47,7 @@ function Main3() {
     };
 
     const titleStyle = {
-        fontSize: '1.5rem', // 타이틀 폰트 사이즈
+        fontSize: '1.5rem', 
         fontWeight: 'bold',
         marginBottom: '10px'
     };
@@ -61,7 +61,7 @@ function Main3() {
         <div style={containerStyle}>
             <div style={descriptionStyle}>많이 사용하는 프로젝트 및 프로세스에 손쉽게 사용할 수 있는 템플릿이 준비되어 있습니다</div>
             <div style={cardsContainerStyle}>
-                <div style={cardStyle}>
+                <div style={cardStyle} className="cardHoverEffect">
                     <div style={titleStyle}>프로젝트 보고서</div>
                     <div style={imagePlaceholderStyle}>이미지 자리</div>
                     <div style={bodyStyle}>
