70b7381a495e1cd2524201e7b9a37ab1eeb88ca6
drimh <107299318+drimh@users.noreply.github.com>
2024-05-25 20:16:08 +0900

    Feat: 로그인 UI 수정(three.js)

--- agile-hub/src/components/LoginPage/KakaoLoginButton.js
+++ agile-hub/src/components/LoginPage/KakaoLoginButton.js
@@ -20,10 +20,9 @@ const KakaoLoginButton = () => {
     window.location.href = 'https://api.agilehub.store/oauth2/authorization/kakao';
   };
 
-
   return (
     <div className="kakao-login-button" onClick={handleLogin}>
-      <img src="../../../assets/images/KakaotalkIcon.png" alt="Kakao Icon" className="kakao-icon"/>
+      <img src="../../../assets/images/KakaotalkIcon.png" alt="Kakao Icon" className="kakao-icon" />
       <span className="kakao-login-text">카카오톡 로그인</span>
     </div>
   );
