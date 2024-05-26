b7a8ef4c1a647c60fb4eb54104449134348b248d
ju wonhee <jwonhee398@gmail.com>
2024-05-26 22:27:48 +0900

    Update User.js

--- agile-hub/src/pages/User.js
+++ agile-hub/src/pages/User.js
@@ -13,12 +13,13 @@ function User() {
     const test = async () => {
         try {
             //const authToken = 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJBZ2lsZUh1YiIsInN1YiI6IkFjY2Vzc1Rva2VuIiwibmFtZSI6IuyLoOyKue2YnCIsInJvbGUiOiJST0xFX1VTRVIiLCJwcm92aWRlciI6Imtha2FvIiwiZGlzdGluY3RJZCI6IjM0NTcyMjMzOTYiLCJpYXQiOjE3MTU1NzM5OTcsImV4cCI6MTcxNjc4MzU5N30.1PRhxReTmFd2UV4CI5tCrDCNq7Re2p9PNslzwfwy0d8ZZbpuxOuKd1FTwjoTkRIwtYmL2V1gzxaDhchatjKhzA'; // Use actual token
-            const response = await axios.get(`https://api.agilehub.store//member/profile`, {
+            const response = await axios.get(`https://api.agilehub.store/member/profile`, {
                 headers: {
                     Authorization: `Bearer ${authToken}`
                 }
             });
             console.log(response);
+            console.log(response.result);
         } catch (error) {
             console.error('API request failed:', error);
         }
