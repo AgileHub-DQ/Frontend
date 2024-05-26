9dc2a87151d43a28911fdbd077858db3e5b16c9a
drimh <107299318+drimh@users.noreply.github.com>
2024-05-26 14:56:53 +0900

    feat: 머지 준비

--- agile-hub/src/components/MyPage/Header.js
+++ agile-hub/src/components/MyPage/Header.js
@@ -1,9 +1,17 @@
 function Header() { 
     const container = { 
+<<<<<<< HEAD
         display: 'flex', 
         alignItems: 'center', 
         width: '100%', 
         height: '100px', 
+=======
+        display: 'flex', // flexbox를 사용하여 요소들을 가로로 정렬합니다.
+        alignItems: 'center', // 수직 중앙 정렬
+        background: 'red',
+        width: '100%', // 전체 너비 사용
+        height: '100px', // 컨테이너 높이 설정
+>>>>>>> d1f2679 (feat: 마이페이지 헤더)
         color: 'var(--kakao-logo, #000)', 
         fontFamily: "Abhaya Libre ExtraBold", 
         fontSize: '1.875rem', 
